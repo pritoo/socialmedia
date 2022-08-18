21a7bd756b595c5b524eb17a4ed5e67dfe5fa5a4
pritoo <preetmangal@gmail.com>
2022-08-18 17:35:23 +0530

    Update userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -85,4 +85,57 @@ exports.login =async(req,res)=>{
             message:error.message
         })
     }
+};
+
+
+exports.followUser = async (req,res)=>{
+  try {
+    const userToFollow = await User.findById(req.params.id);
+    const loggedInUser = await User.findById(req.user._id);
+
+    if(!userToFollow){
+      return res.status(404).json({
+        success:false,
+        message:"User not found"
+      })
+    
+    }
+    if(loggedInUser.following.includes(userToFollow._id)){
+      const indexfollowing= loggedInUser.following.indexOf(userToFollow._id);
+      const indexfollowers = userToFollow.followers.indexOf(loggedInUser._id);
+
+      loggedInUser.following.splice(indexfollowing,1);
+      userToFollow.followers.splice(indexfollowers,1);
+
+      await loggedInUser.save();
+      await userToFollow.save();
+
+      res.status(200).json({
+        success:true,
+        message:"User Unfollowed"
+      });
+    }
+    else{
+      loggedInUser.following.push(userToFollow._id);
+      //console.log(userToFollow._id)
+      userToFollow.followers.push(loggedInUser._id);
+     //console.log(loggedInUser._id)
+
+      await loggedInUser.save();
+      await userToFollow.save();
+
+      return res.status(200).json({
+        success:true,
+        message:"User followed"
+      })
+      
+    }
+    
+  } catch (error) {
+    res.status(500).json({
+      success:false,
+      message:error.message
+    })
+    
+  }
 }
