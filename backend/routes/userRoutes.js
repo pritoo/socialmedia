23370d98fea7c812dc17bde6d321fa34f717b31a
pritoo <preetmangal@gmail.com>
2022-08-18 17:35:57 +0530

    Update userRoutes.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,14 @@
 const express = require("express");
-const { register,login } = require("../controllers/userController");
+const { register,login, followUser } = require("../controllers/userController");
+const { isAuthenticated } = require("../middleware/auth");
 
 const router = express.Router();
 
 
 router.route("/register").post(register);
 
-router.route("/login").get(login)
+router.route("/login").get(login);
+
+router.route("/follow/:id").get(isAuthenticated,followUser)
 
 module.exports = router
