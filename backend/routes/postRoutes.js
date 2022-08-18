332491cd64b19034df087b27d05727832a9d19e4
pritoo <preetmangal@gmail.com>
2022-08-18 17:36:22 +0530

    Update postRoutes.js

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,7 +1,12 @@
 const express = require("express")
-const {createPost} =require("../controllers/postController")
+const {createPost,deletePost, likeAndUnlikePost} =require("../controllers/postController")
 const {isAuthenticated} = require("../middleware/auth")
 const router = express.Router();
 
-router.route("/post/upload").post(isAuthenticated,createPost)
+router.route("/post/upload").post(isAuthenticated,createPost);
+
+router.route("/post/:id").get(isAuthenticated,likeAndUnlikePost);
+
+router.route("/post/:id").delete(isAuthenticated,deletePost);
+
 module.exports = router
